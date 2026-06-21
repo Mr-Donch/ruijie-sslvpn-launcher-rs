7d427b16694354ee808244c74cbde3ecc2867d04
dongchao <dongchao@ghostcloud.cn>
2026-06-21 16:44:19 +0800

    feat: 嵌入从 RG-SSLVPN.exe 提取的应用图标

--- build.rs
+++ build.rs
@@ -3,10 +3,20 @@ fn main() {
         return;
     }
 
+    embed_application_icon();
+
     if std::env::var("PROFILE").as_deref() != Ok("release") {
         return;
     }
 
     println!("cargo:rustc-link-arg-bin=ruijie-sslvpn-launcher-rs=/MANIFEST:EMBED");
     println!("cargo:rustc-link-arg-bin=ruijie-sslvpn-launcher-rs=/MANIFESTUAC:level='requireAdministrator' uiAccess='false'");
+}
+
+fn embed_application_icon() {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("assets/RG-SSLVPN.ico");
+    if let Err(err) = res.compile() {
+        eprintln!("warning: failed to compile icon resource: {err}");
+    }
 }
